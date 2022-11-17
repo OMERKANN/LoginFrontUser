0217c413fdc9f8688b076a949295be00f5ad42ff
Omer Kan <omerkan93@gmail.com>
2022-11-17 15:41:56 +0300

    createBpdyParser

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require("express")
 const bodyParser = require("body-parser");
 
-
-
 const app = express();
 
-
 app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html")
 })
 
+app.post("/", (req, res) =>{
+
+    const mail = req.body.eMail;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    console.log(mail, firstName, lastName );
+
+})
 
 app.listen(3000, function(req, res){
     console.log("Start Server")
