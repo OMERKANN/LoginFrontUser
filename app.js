0d7af51e1b791f7509d9d8812d8f0b630040137c
Omer Kan <omerkan93@gmail.com>
2022-11-17 15:22:06 +0300

    createLoginData

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const bodyParser = require("body-parser");
+
+
+
+const app = express();
+
+
+app.use(express.static('public'))
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html")
+})
+
+
+app.listen(3000, function(req, res){
+    console.log("Start Server")
+})
